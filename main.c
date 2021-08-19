6ee3771a9054b36858ae08c1efd70edb41514b90
DavidCG07 <cbi2173047251@izt.uam.mx>
2021-08-19 18:01:24 -0500

    Hola

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+nombre del programa
+Autor
+Modificado por
+que hace el programa
+necesita un circuito
+colocar una imagen en el repositorio_liga
+terminales que se utilizan, el modo
+ejemplos: gpio 5_salida*/
+
+/*Bibliotecas*/
+#include <stdio.h>
+#include<stdlib.h>
+
+/*declaracion de las variables con el tipo correspondiente*/
+
+/*estructura main*/
+
+int main(){
+
+    printf("Hola UAM\n");
+    return 0;
+}
